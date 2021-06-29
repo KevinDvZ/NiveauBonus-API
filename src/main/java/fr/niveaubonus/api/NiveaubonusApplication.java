134bebc2dea9f4a5bf01c62827b226eaa6100bef
Kevin DvZ <kevin.deveza@gmail.com>
2021-06-29 11:35:19 +0200

    first commit

---
+++ src/main/java/fr/niveaubonus/api/NiveaubonusApplication.java
@@ -0,0 +1,13 @@
+package fr.niveaubonus.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiveaubonusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiveaubonusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/niveaubonus/api/NiveaubonusApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.niveaubonus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiveaubonusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
